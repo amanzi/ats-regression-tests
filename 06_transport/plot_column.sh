b92bb2b439363811968474cd4a827d1c73bb9410
Ethan Coon <coonet@ornl.gov>
2020-09-14 16:04:49 -0400

    adds diffusion test, which also shows an issue in coupling surface and subsurface

---
+++ 06_transport/plot_column.sh
@@ -0,0 +1,1 @@
+python $ATS_SRC_DIR/tools/utils/plot_column_data.py --color-mode=time --layout="[[surface-pressure,surface-ponded_depth,surface-total_component_concentration],[pressure,saturation_liquid,total_component_concentration]]" .
