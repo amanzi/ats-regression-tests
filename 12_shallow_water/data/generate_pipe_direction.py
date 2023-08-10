7742df4ce98333c4d0e975829f7e073a85ec6409
Giacomo Capodaglio <gcapodaglio@pn2003029.lanl.gov>
2023-08-10 17:05:39 -0600

    Added "direction key" entry to read it in
    From file. This is in coupled_surface_pipe_model.xml

--- 12_shallow_water/data/generate_pipe_direction.py
+++ 12_shallow_water/data/generate_pipe_direction.py
@@ -1,18 +1,30 @@
 import h5py
 import numpy as np
 
-filename = "test.h5"
+filename = "pipe_direction.h5"
 
 hfw= h5py.File(filename,"w")
 
 g1 = hfw.create_group('pipe-direction.cell.0')
-n1 = np.zeros((100,2))
+size = 50
+n1 = np.zeros((size,1))
 
-for i in range(100):
-    n1[i][0]=1
+for i in range(size):
+    n1[i][0]=2
 
 
 g1.create_dataset('0',data=n1)
+
+g1 = hfw.create_group('pipe-direction.cell.1')
+size = 50
+n1 = np.zeros((size,1))
+
+for i in range(size):
+    n1[i][0]=0
+
+
+g1.create_dataset('0',data=n1)
+
 
 hfw.close()
 
