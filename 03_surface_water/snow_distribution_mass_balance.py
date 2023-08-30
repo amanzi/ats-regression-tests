b91b5a08d64eabc17c04c74c665f0e11c38022d4
Joe Beisman <jjbeisman@gmail.com>
2023-08-30 06:55:36 -0400

    Removed mass balance script.

--- 03_surface_water/snow_distribution_mass_balance.py
+++
@@ -1,7 +0,0 @@
-import netCDF4
-import numpy as np
-dat = netCDF4.Dataset('checkpoint00001.h5')
-distributed_precip_rate = dat.variables['snow-precipitation.cell.0']
-precip_forcing_rate = 1.0e-5
-rel_error = (precip_forcing_rate - np.average(distributed_precip_rate)) / precip_forcing_rate
-print('The relative error in precipitation distributiion is ', rel_error)
