f43d1fc8ebf64019aa8aa81ba63e08804804de3e
Ethan Coon <coonet@ornl.gov>
2025-02-18 16:30:02 -0700

    updates all reactive transport tests, now running though probably not passing

--- 06_transport/plot_surface_tracer.py
+++ 06_transport/plot_surface_tracer.py
@@ -5,25 +5,33 @@ import colors
 
 import sys,os
 
-def plot(dname):
-    f = ats_xdmf.VisFile(dname, 'surface')
+def plot(dname, domain):
+    f = ats_xdmf.VisFile(dname, domain)
     f.loadMesh(columnar=True)
 
-    tcc1 = f.getArray('surface-total_component_concentration.Tracer1')
-    tcc2 = f.getArray('surface-total_component_concentration.Tracer2')
-
-    cm1 = colors.cm_mapper(0, tcc1.shape[0]-1, 'Reds')
-    cm2 = colors.cm_mapper(0, tcc2.shape[0]-1, 'Blues')
-
+    comps = f.search("total_component_concentration")
     x = f.centroids[:,0,0]
-    
     fig, ax = plt.subplots(1,1)
-    for i in range(0, tcc1.shape[0]+1, 10):
-        ax.plot(x, tcc1[i,:,0], color=cm1(i))
-        ax.plot(x, tcc2[i,:,0], color=cm2(i))
+
+    if len(comps) <= 2:
+        cmaps = ['jet', 'viridis_r']
+    else:
+        cmaps = ['Blues', 'Reds', 'Purples', 'Oranges', 'Greens', 'Greys']
+
+    for i, comp in enumerate(comps):
+        print(comp)
+        tcc = f.getArray(comp)
+        cm = colors.cm_mapper(0, tcc.shape[0]-1, cmaps[i])
+    
+        for i in range(0, tcc.shape[0]+1, 10):
+            ax.plot(x, tcc[i,:,0], color=cm(i))
 
     plt.show()
 
 
 if __name__ == '__main__':
-    plot(sys.argv[-1])
+    if sys.argv[-2] == "-s":
+        domain = None
+    else:
+        domain = 'surface'
+    plot(sys.argv[-1], domain)
