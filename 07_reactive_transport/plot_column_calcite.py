97cfd229996eeba3d0b5f4d837de85c42c95134d
Ethan Coon <coonet@ornl.gov>
2025-04-15 16:43:56 -0600

    changes names from tcc to molar_mixing_ratio in ats transport

--- 07_reactive_transport/plot_column_calcite.py
+++ 07_reactive_transport/plot_column_calcite.py
@@ -8,10 +8,28 @@ if __name__ == '__main__':
     if not os.path.isdir(dname):
         raise RuntimeError("USAGE: python plot_column_calcite.py DIRNAME")
 
-    axs = plot_column_data.plotColumnData([dname,],
-                                          color_mode='time',
-                                          layout="[[surface-pressure,surface-ponded_depth,surface-total_component_concentration.H+,surface-total_component_concentration.HCO3-,surface-total_component_concentration.Ca++,surface-total_component_concentration.Tracer],[pressure,saturation_liquid,total_component_concentration.H+,total_component_concentration.HCO3-,total_component_concentration.Ca++,total_component_concentration.Tracer]]",
-                                          figsize=(12,8))
+    hco3_key = 'HCO3-'
 
+    fig = plt.figure(figsize=(12,8))
+    
+    def plot(hco3_key, surf_hco3_key):
+        axs = plot_column_data.plotColumnData([dname,],
+                                              color_mode='time',
+                                              layout=f"[[surface-pressure,surface-ponded_depth,surface-total_component_concentration.H+,surface-total_component_concentration.{surf_hco3_key},surface-total_component_concentration.Ca++,surface-total_component_concentration.Tracer],[pressure,saturation_liquid,total_component_concentration.H+,total_component_concentration.{hco3_key},total_component_concentration.Ca++,total_component_concentration.Tracer]]",
+                                              fig=fig)
+
+    try:
+        plot('HCO3-', 'HCO3-')
+    except KeyError:
+        plt.clf()
+        try:
+            plot('HCO3', 'HCO3-')
+        except KeyError:
+            plt.clf()
+            try:            
+                plot('HCO3', 'HCO3')
+            except KeyError:
+                plt.clf()
+                plot('HCO3-', 'HCO3')
     
     plt.show()
